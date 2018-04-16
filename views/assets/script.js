1dea73715b0e5e962b84991cbade15a40825287e
otharwa <otharwa@navebinario.com>
2018-04-15 21:12:00 -0300

    Frontend - Logica en JS
     - Rellena las opciones de matrices disponibles (select)
     - Realiza consulta contra el api
     - Parsea el resultado y muestra tanto el resultado como la matriz usada

---
+++ views/assets/script.js
@@ -0,0 +1,45 @@
+(function(){
+	var that			= this;
+	this.matrixOptions	= [];
+	$('#answer').hide();
+
+	Object.keys(fullMatrix).forEach(function(value){
+		var name	= value.split('_');
+		that.matrixOptions.push($('<option value="'+value+'">' + name[0] + ' fila de ' + name[1] + ' letras</option>'));
+	});
+
+	$('#inputGroupSelect01').append(this.matrixOptions);
+
+	this.printAnswer	= function(res){
+		console.log(typeof res);
+		console.log(typeof res.countWordAppers);
+		if(typeof res.countWordAppers !== 'number')
+			return;
+		$('#answer_number').html(res.countWordAppers);
+
+		var matrixToText	= [];
+		fullMatrix[res.selectedMatrix].forEach(function(row,i){
+			matrixToText.push($('<p>' + row.toString() + '</p>'))
+		});
+		$('#matrix_content').html('');
+		$('#matrix_content').append(matrixToText);
+		$('#answer').show();
+	}
+	this.callApi			= function(){
+		$.ajax({
+			url: './api.php',
+			data: {
+				selectedMatrix: $('#inputGroupSelect01').val(),
+			},
+			dataType: 'json',
+			success: function(data, status){
+				that.printAnswer(data);
+				console.log(status);
+				console.log(data);
+			}
+		});
+		return false;
+	}
+
+	$('#calculate').click(this.callApi);
+})();
